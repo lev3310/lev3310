aa5d2ed5064c51d164ed00ca994b67483d27c1dc
Lev3310 <lolkatrolka3310+github1@gmail.com>
2020-04-15 15:26:18 +0200

    1

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 $(document).ready(function () {
     $('#switch-player-btn').on('click', function(){
-        $('#switch-player').html() == "1" ? $('#switch-player').html('2') : $('#switch-player').html('1');
-    });
-})
+//        $('#switch-player').html() == "1" ? $('#switch-player').html('2') : $('#switch-player').html('1');
+	if ($('#switch-player').html() == "1") {
+			$('#switch-player').html('2');
+			$('#switch-player-btn').css("background-color", "#ff0000");
+		}
+		else {
+			$('#switch-player-btn').css("background-color", "#0014ff");
+			$('#switch-player').html('1');
+		}
+	});
+});

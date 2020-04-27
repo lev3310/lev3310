bc32bc2b4a4b930baf5188c1db7b5e9069a2ae4f
Lev3310 <lolkatrolka3310+github1@gmail.com>
2020-04-27 19:46:14 +0200

    1

--- script.js
+++ script.js
@@ -1,32 +1,23 @@
-//$(document).ready(function () {
-//    $('#switch-player-btn').on('click', function(){
-///        $('#switch-player').html() == "1" ? $('#switch-player').html('2') : $('#switch-player').html('1');
-//	if ($('#switch-player').html() == "1") {
-//			$('#switch-player').html('2');
-//			$('#switch-player-btn').css("background-color", "#000000");
-//		}
-//		else {
-//		$('#switch-player-btn').css("background-color", "#ebe8ec");
-//			$('#switch-player').html('1');
-//		}
-//	});
-// });
-
 $(document).ready(function () {
 	var player = 1;
 	var state = "state_1_js";
 $('#switch-player-btn').on('click',function(){
-	$("chest-input").each(function(i,element){
+	$(".chest-input").each(function(i,element){
 		$(element).attr('disabled', 'disabled');
 	});
-	var switchPlayer = $('switch-player');
+	var switchPlayer = $('#switch-player');
+	console.log(switchPlayer.html());
 	if (switchPlayer.html() == "1") {
 		$(switchPlayer).html('2');
-		$(this).toggleClass("state_1_js").toggleClass(state_2_js);
+		$(this).toggleClass("state_1_js").toggleClass("state_2_js");
 		player = 2;
-		state = "state_1_js";
-	}
-	$(".chest-input").not(":checked").not("state_1_js").not("state_2_js").slice(0.4).removeAttr("disabled");
+		state = "state_2_js";
+	} else {
+            $(switchPlayer).html('1');
+            $(this).toggleClass("state_1_js").toggleClass("state_2_js");
+            player = 1;
+            state = "state_1_js";
+        }$(".chest-input").not(":checked").slice(0,4).removeAttr("disabled");
 });
 
 $('.chest-input').change(function() {
@@ -36,5 +27,8 @@ $('.chest-input').change(function() {
 	else {
 		$(this).removeClass(state);
 	};
-});
+	});
+	$('#last-element').change(function() {
+	alert("sorry but, ты проиграл");
+	});
 });
